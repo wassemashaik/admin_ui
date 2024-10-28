16cb0d43aa4df9606cb895b0bbd8b8859ca6d015
wassema <swassema@gmail.com>
2024-10-28 22:55:18 +0530

    admin UI commiting

--- README.md
+++ README.md
@@ -1,70 +1,56 @@
-# Getting Started with Create React App
+# Title
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+    Admin UI
 
-## Available Scripts
+## Objective
 
-In the project directory, you can run:
+    Work at a startup that is building an interface for admins to see and delete users.
 
-### `npm start`
+## Tech Stack
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+    React JS
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+## Completion Instructions
 
-### `npm test`
+### Functionality
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+#### Must Have
 
-### `npm run build`
+-   Search box placeholder text should start with Search.
+-   Search icon/button should have class as search-icon OR trigger search on ENTER.
+-   Action element must be a button with a specific class name like edit, delete, save.
+-   Navigation elements must be a div/button, and should have class name as first-page, previous-page, next-page and last-page and page numbers should be mentioned accordingly.
+-   On clicking edit action in a row, it should be editable in the row itself.
+-   Bottom delete button must have text Delete Selected.
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### Guidelines to develop a project
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+#### Must Have
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+-  Column titles must stand out from the entries.
+-   There should be a search bar that can filter on any property.
+-   You should be able to edit or delete rows in place.
+-   You need to implement pagination:  Each page contains 10 rows. 
+-   You should be able to select one or more rows. 
+-   A selected row is highlighted with a grayish background color. 
+-    Multiple selected rows can be deleted at once using the 'Delete Selected' button at the bottom left.
+-   Avoid using libraries like material UI and bootstrap for basic html components like buttons, checkboxes, textbox etc.
 
-### `npm run eject`
+### Submission Instructions
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+#### Must Have
+-   Search box placeholder text should start with Search.
+-   Search icon/button should have class as search-icon OR trigger search on ENTER.
+-   Action element must be a button with a specific class name like edit, delete, save.
+-   Navigation elements must be a div/button, and should have class name as first-page, previous-page, next-page and last-page and page numbers should be mentioned accordingly.
+-   On clicking edit action in a row, it should be editable in the row itself.
+-   Bottom delete button must have text Delete Selected.
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+## Resources
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+### APIs
+-   https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+### Third-party packages
 
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+-   react-icons
---
+++ src/components/Home/index.js
@@ -0,0 +1,233 @@
+import React, { useEffect, useState } from "react";
+import { MdDeleteOutline } from "react-icons/md";
+import { FaRegEdit } from "react-icons/fa";
+import { CiSaveDown2 } from "react-icons/ci";
+import "./index.css";
+
+export default function Home() {
+  const [userData, setUserData] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedItem, setSelectedItem] = useState([]);
+  const [editableData, setEditableData] = useState({
+    name: "",
+    email: "",
+    role: "",
+  });
+  const itemsPerPage = 10;
+
+  const getList = async () => {
+    const apiUrl =
+      "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json";
+    const options = {
+      method: "GET",
+    };
+    const response = await fetch(apiUrl, options);
+    const data = await response.json();
+    if (response.ok === true) {
+      const updatedData = data.map((user) => ({
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      }));
+      setUserData(updatedData);
+    }
+  };
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const handleEdit = (id, newName) => {
+    const updatedList = userData.map((item) =>
+      item.id === id ? { ...item, name: newName } : item
+    );
+    setUserData(updatedList);
+  };
+
+  const handleDelete = (id) => {
+    const updatedList = userData.filter((item) => item.id !== id);
+    setUserData(updatedList);
+  };
+
+  const handleEditing = (id) => {
+    setIsEditing(id);
+    const item = filteredSearch.find((user) => user.id === id);
+    setEditableData({ name: item.name, email: item.email, role: item.role });
+  };
+
+  const handleInputChange = (field, value) => {
+    setEditableData((prevData) => ({ ...prevData, [field]: value }));
+  };
+
+  const handleSave = (id) => {
+    setUserData((prevData) =>
+      prevData.map((item) =>
+        item.id === id ? { ...item, ...editableData } : item
+      )
+    );
+    setIsEditing(null);
+  };
+
+  const itemSelected = (id) => {
+    setSelectedItem((prevSelected) => {
+      if (prevSelected.includes(id)) {
+        return prevSelected.filter((selectedId) => selectedId !== id);
+      } else {
+        return [...prevSelected, id];
+      }
+    });
+  };
+
+  const inputChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const deleteSelectedItems = () => {
+    const updatedList = userData.filter(
+      (item) => !selectedItem.includes(item.id)
+    );
+    setUserData(updatedList);
+    setSelectedItem([]);
+  };
+
+  const filteredSearch = userData.filter((item) => {
+    const matchedSearch =
+      item.name.toLowerCase().includes(searchInput.toLowerCase()) ||
+      item.email.toLowerCase().includes(searchInput.toLowerCase()) ||
+      item.role.toLowerCase().includes(searchInput.toLowerCase());
+    return matchedSearch;
+  });
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredSearch.slice(indexOfFirstItem, indexOfLastItem);
+  
+  const totalPages = Math.ceil(filteredSearch.length / itemsPerPage);
+
+  const renderUserDetails = () => (
+    <div className="container">
+      <input
+        type="text"
+        value={searchInput}
+        onChange={inputChange}
+        className="search"
+        placeholder="Search"
+      />
+      <table>
+        <tr className="list-item">
+          <th></th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          <th>Actions</th>
+        </tr>
+        <ul className="list-container">
+          {currentItems.map((user) => (
+            <tr key={user.id} className="list-item">
+              <td>
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  onChange={() => itemSelected(user.id)}
+                  checked={selectedItem.includes(user.id)}
+                />
+              </td>
+              <td className="heading">
+                {isEditing === user.id ? (
+                  <input
+                    className="input-element"
+                    type="text"
+                    value={editableData.name}
+                    onChange={(e) => handleInputChange("name", e.target.value)}
+                  />
+                ) : (
+                  <span>{user.name}</span>
+                )}
+              </td>
+              <td className="para">
+                {isEditing === user.id ? (
+                  <input
+                    className="input-element"
+                    type="text"
+                    value={editableData.email}
+                    onChange={(e) => handleInputChange("email", e.target.value)}
+                  />
+                ) : (
+                  <span>{user.email}</span>
+                )}
+              </td>
+              <td className="para">
+                {isEditing === user.id ? (
+                  <input
+                    className="input-element"
+                    type="text"
+                    value={editableData.role}
+                    onChange={(e) => handleInputChange("role", e.target.value)}
+                  />
+                ) : (
+                  <span>{user.role}</span>
+                )}
+              </td>
+              <td className="action">
+                <div className="btn-container">
+                  <button
+                    className="edit-button"
+                    onChange={(e) => handleEdit(user.id, e.target.value)}
+                    onClick={() =>
+                      isEditing === user.id
+                        ? handleSave(user.id)
+                        : handleEditing(user.id)
+                    }
+                  >
+                    {isEditing === user.id ? <CiSaveDown2 className="save" /> : <FaRegEdit className="edit" />}
+                  </button>
+                  <button
+                    className="delete"
+                    onClick={() => handleDelete(user.id)}
+                  >
+                    <MdDeleteOutline />
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </ul>
+      </table>
+      <div>
+        <button
+          className="prev-page"
+          onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+          disabled={currentPage === 1}
+        >
+          prev
+        </button>
+        {[...Array(totalPages)].map((_, i) => (
+          <button
+            className={`first-page ${currentPage === i + 1 ? "active" : ""}`}
+            key={i + 1}
+            onClick={() => setCurrentPage(i + 1)}
+          >
+            {i + 1}
+          </button>
+        ))}
+        <button
+        className={`next-page ${currentPage === totalPages ? "disabled" : ""}`}
+          onClick={() =>
+            setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+          }
+          disabled={currentPage === totalPages}
+        >
+          next
+        </button>
+      </div>
+      <button onClick={deleteSelectedItems} className="delete-selected">
+        Delete Selected
+      </button>
+    </div>
+  );
+
+  return <div>{renderUserDetails()}</div>;
+}
